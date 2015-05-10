288bd896a6bd6b7cb3f302c7b6dc4a51477cc9f0
sng <sng@mindtangle.com>
2015-05-10 03:57:59 -0500

    Added function to send a message string with send button and return the message in another screen

--- app/src/main/java/com/mindtangle/sng/testandroidapp/MainActivity.java
+++ app/src/main/java/com/mindtangle/sng/testandroidapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.mindtangle.sng.testandroidapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mindtangle.sng.testandroidapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +39,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //** Called when user clicks send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
